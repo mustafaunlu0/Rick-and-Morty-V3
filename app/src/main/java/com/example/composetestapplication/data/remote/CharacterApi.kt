05f7e72292a2f82ce1ed57d685ccf6f21bdd58d1
erlab <mustafa.unlu@erlab.com>
2025-03-10 16:56:41 +0300

    Added the retrofit api service.

---
+++ app/src/main/java/com/example/composetestapplication/data/remote/CharacterApi.kt
@@ -0,0 +1,17 @@
+package com.example.composetestapplication.data.remote
+
+
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface CharacterApi {
+
+    @GET("character")
+    suspend fun getCharacters() : ResponseBody
+
+    @GET("character/{characterNumber}")
+    suspend fun getCharacterDetail(
+        @Path("characterNumber") characterNumber : String
+    ) : ResponseBody
+}
