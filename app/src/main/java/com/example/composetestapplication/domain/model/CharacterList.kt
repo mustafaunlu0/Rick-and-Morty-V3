bb072cc70ca6cc13d04a9de642bf871a332628dc
erlab <mustafa.unlu@erlab.com>
2025-03-10 16:56:59 +0300

    Added the domain(ui) model.

---
+++ app/src/main/java/com/example/composetestapplication/domain/model/CharacterList.kt
@@ -0,0 +1,8 @@
+package com.example.composetestapplication.domain.model
+
+data class CharacterList(
+    val id : Int,
+    val name : String,
+    val species : String,
+    val image : String,
+)
