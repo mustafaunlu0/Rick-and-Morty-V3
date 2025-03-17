adb5459bbe47d542e1fbe12f628203b1759d3b51
Mustafa Ünlü <mustafa.unlu@erlab.com>
2025-03-17 14:15:51 +0300

    Edited the text length.

--- app/src/main/java/com/example/composetestapplication/presentation/list_screen/component/CharacterItem.kt
+++ app/src/main/java/com/example/composetestapplication/presentation/list_screen/component/CharacterItem.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -19,8 +18,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
@@ -60,19 +59,14 @@ fun CharacterItem(
                 verticalArrangement = Arrangement.SpaceEvenly,
                 modifier = Modifier
                     .fillMaxHeight()
-                    .padding(horizontal = 10.dp)
+                    .padding(horizontal = 10.dp).weight(1f)
             ) {
-                Text(characterListing.name, fontSize = 18.sp)
+                Text(characterListing.name, fontSize = 18.sp, maxLines =  1,   overflow = TextOverflow.Ellipsis )
 
                 Text(characterListing.species, fontSize = 14.sp)
 
-                //Text(characterListing., fontSize = 12.sp)
             }
-
-            Spacer(modifier = Modifier.weight(1f))
-
             Text(text = characterListing.id.toString(), fontSize = 62.sp, modifier = Modifier.padding(end = 10.dp),)
-
         }
 
     }
